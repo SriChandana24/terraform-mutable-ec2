efd9dc205a506f81c44a7c02465c8bd20f33a926
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-24 09:19:36 +0530

    Update terraform-modules/terraform-mutable-ec2/

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 output "EBS" {
-  value = lookup(tomap(element(aws_spot_instance_request.spot-instance.*.root_block_device, 0)), "volume_id", null)
+  value = lookup(element(aws_spot_instance_request.spot-instance.*.root_block_device, 0), "volume_id", null)
 }
 
 // lookup(map, key, default)
