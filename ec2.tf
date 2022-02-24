c69ffc3af94117c925d5156b76d32af02a3ef72f
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-24 08:43:40 +0530

    Update terraform-modules/terraform-mutable-ec2/

--- ec2.tf
+++ ec2.tf
@@ -3,3 +3,11 @@ resource "aws_instance" "od-instance" {
   instance_type = var.OD_INSTANCE_TYPE
   ami           = data.aws_ami.ami.id
 }
+
+
+resource "aws_spot_instance_request" "spot-instance" {
+  count                = var.SPOT_INSTANCE_COUNT
+  ami                  = data.aws_ami.ami.id
+  instance_type        = var.SPOT_INSTANCE_TYPE
+  wait_for_fulfillment = true
+}
