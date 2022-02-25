3a4bef5ae14fa79fb9fff30e30675d0f3658213d
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-25 08:02:13 +0530

    Update terraform-modules/terraform-mutable-ec2/

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
 }
 //
 output "EBS" {
-  value = lookup(aws_spot_instance_request.spot-instance.*.root_block_device[0], "volume_id")
+  value = lookup(tomap(aws_spot_instance_request.spot-instance.*.root_block_device[0]), "volume_id")
 }
 //
 //// lookup(map, key, default)
