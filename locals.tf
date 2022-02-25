9d347c36b583599640f4121d0ef509bdfc37fe34
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-25 08:17:46 +0530

    Update terraform-modules/terraform-mutable-ec2/

--- locals.tf
+++ locals.tf
@@ -2,16 +2,12 @@ locals {
   ALL_INSTANCE_IDS = concat(aws_instance.od-instance.*.id, aws_spot_instance_request.spot-instance.*.spot_instance_id)
   ALL_SPOT_IDS     = aws_spot_instance_request.spot-instance.*.id
   #ALL_EBS_IDS = aws_spot_instance_request.spot-instance.*.ebs_block_device
-  ALL_TAG_IDS = concat(local.ALL_INSTANCE_IDS, local.ALL_SPOT_IDS)
-  TAG_NAME    = "${var.COMPONENT}-${var.ENV}"
+  ALL_TAG_IDS    = concat(local.ALL_INSTANCE_IDS, local.ALL_SPOT_IDS)
+  TAG_NAME       = "${var.COMPONENT}-${var.ENV}"
+  SPOT_VOLUME_ID = [for m in flatten(aws_spot_instance_request.spot-instance.*.root_block_device) : m.volume_id]
 }
-//
+
+
 output "EBS" {
-  value = [for m in flatten(aws_spot_instance_request.spot-instance.*.root_block_device) : m.volume_id]
+  value = aws_instance.od-instance
 }
-
-
-//
-//// lookup(map, key, default)
-
-//value = "${lookup(data.aws_kms_secrets.api_key.*.plaintext[count.index], "secret_name")}"
