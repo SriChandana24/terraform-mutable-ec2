9d2ffc01c6d34707e33320728fc801304263e33b
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-26 09:06:16 +0530

    Update terraform-modules/terraform-mutable-ec2/

--- tags.tf
+++ tags.tf
@@ -1,10 +1,10 @@
-module "tags" {
-  count       = length(local.ALL_TAGS)
-  source      = "git::https://github.com/raghudevopsb62/terraform-tags"
-  TAG_NAME    = lookup(element(local.ALL_TAGS, count.index), "name")
-  TAG_VALUE   = lookup(element(local.ALL_TAGS, count.index), "value")
-  RESOURCE_ID = local.ALL_TAG_IDS
-}
+//module "tags" {
+//  count       = length(local.ALL_TAGS)
+//  source      = "git::https://github.com/raghudevopsb62/terraform-tags"
+//  TAG_NAME    = lookup(element(local.ALL_TAGS, count.index), "name")
+//  TAG_VALUE   = lookup(element(local.ALL_TAGS, count.index), "value")
+//  RESOURCE_ID = local.ALL_TAG_IDS
+//}
 
 locals {
   ALL_TAGS = [
@@ -26,3 +26,4 @@ locals {
     }
   ]
 }
+
