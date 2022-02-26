8a0fac6bfc563a9c143414c3fff7fa76e793ff87
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-26 08:57:57 +0530

    Update terraform-modules/terraform-mutable-ec2/

--- data.tf
+++ data.tf
@@ -3,3 +3,14 @@ data "aws_ami" "ami" {
   name_regex  = "base-with-ansible"
   owners      = ["self"]
 }
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-b62"
+    key    = "mutable/vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
