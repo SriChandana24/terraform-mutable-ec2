8951cb7da7158d28ad005a743f9889bbe21c0350
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-28 09:05:31 +0530

    Update terraform-modules/terraform-mutable-ec2/

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,19 @@
 resource "aws_instance" "od-instance" {
-  count         = var.OD_INSTANCE_COUNT
-  instance_type = var.OD_INSTANCE_TYPE
-  ami           = data.aws_ami.ami.id
-  subnet_id     = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
+  count                  = var.OD_INSTANCE_COUNT
+  instance_type          = var.OD_INSTANCE_TYPE
+  ami                    = data.aws_ami.ami.id
+  subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
+  vpc_security_group_ids = [aws_security_group.allow.id]
 }
 
 resource "aws_spot_instance_request" "spot-instance" {
-  count                = var.SPOT_INSTANCE_COUNT
-  ami                  = data.aws_ami.ami.id
-  instance_type        = var.SPOT_INSTANCE_TYPE
-  wait_for_fulfillment = true
-  subnet_id            = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
+  count                  = var.SPOT_INSTANCE_COUNT
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.SPOT_INSTANCE_TYPE
+  wait_for_fulfillment   = true
+  subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
+  vpc_security_group_ids = [aws_security_group.allow.id]
+
   //  root_block_device {
   //    volume_size = 10
   //  }
