c0d18e1e8f6eeef8adb3fd7a611f23fd180d6031
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-03 08:19:17 +0530

    Update terraform-modules/terraform-mutable-ec2/

--- alb.tf
+++ alb.tf
@@ -31,14 +31,27 @@ resource "aws_lb_listener" "frontend" {
   }
 }
 
+resource "random_integer" "priority" {
+  min = 100
+  max = 500
+  keepers = {
+    listener_arn = data.terraform_remote_state.alb.outputs.backend_listener_arn
+  }
+}
 
-resource "aws_lb_listener" "backend" {
-  count             = var.ALB_ATTACH_TO == "backend" ? 1 : 0
-  load_balancer_arn = data.terraform_remote_state.alb.outputs.frontend-arn
-  port              = "8080"
-  protocol          = "HTTP"
-  default_action {
+resource "aws_lb_listener_rule" "static" {
+  count        = var.ALB_ATTACH_TO == "backend" ? 1 : 0
+  listener_arn = data.terraform_remote_state.alb.outputs.backend_listener_arn
+  priority     = random_integer.priority.result
+
+  action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.roboshop.internal"]
+    }
   }
 }
