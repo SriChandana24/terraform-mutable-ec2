5b53ca8e6f064e4bf20fe4745f4db42490eafaca
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-16 09:29:11 +0530

    Update terraform-modules/terraform-mutable-ec2/

--- awx.tf
+++ awx.tf
@@ -1,11 +1,11 @@
-resource "awx_host" "k3snode1" {
-  name         = "cart"
-  description  = "cart"
-  inventory_id = 3
-  group_ids    = []
-  enabled      = true
-  variables    = <<YAML
----
-ansible_host: 192.168.178.29
-YAML
-}
+//resource "awx_host" "k3snode1" {
+//  name         = "cart"
+//  description  = "cart"
+//  inventory_id = 3
+//  group_ids    = []
+//  enabled      = true
+//  variables    = <<YAML
+//---
+//ansible_host: 192.168.178.29
+//YAML
+//}
