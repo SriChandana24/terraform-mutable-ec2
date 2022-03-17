8a758d9f8e3e8a9bc67da19a0049a06836c36dc9
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-17 07:49:27 +0530

    Update terraform-modules/terraform-mutable-ec2/

--- ec2.tf
+++ ec2.tf
@@ -15,21 +15,21 @@ resource "aws_spot_instance_request" "spot-instance" {
   vpc_security_group_ids = [aws_security_group.allow.id]
 }
 
-//resource "null_resource" "ansible-apply" {
-//
-//  triggers = {
-//    TRIGGER = var.TRIGGER
-//  }
-//
-//  count = length(local.ALL_INSTANCE_IPS)
-//  provisioner "remote-exec" {
-//    connection {
-//      host     = element(local.ALL_INSTANCE_IPS, count.index)
-//      user     = jsondecode(data.aws_secretsmanager_secret_version.latest.secret_string)["SSH_USER"]
-//      password = jsondecode(data.aws_secretsmanager_secret_version.latest.secret_string)["SSH_PASS"]
-//    }
-//    inline = [
-//      "ansible-pull -U https://github.com/raghudevopsb62/ansible roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV}"
-//    ]
-//  }
-//}
+resource "null_resource" "ansible-apply" {
+
+  triggers = {
+    TRIGGER = var.TRIGGER
+  }
+
+  count = length(local.ALL_INSTANCE_IPS)
+  provisioner "remote-exec" {
+    connection {
+      host     = element(local.ALL_INSTANCE_IPS, count.index)
+      user     = jsondecode(data.aws_secretsmanager_secret_version.latest.secret_string)["SSH_USER"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.latest.secret_string)["SSH_PASS"]
+    }
+    inline = [
+      "ansible-pull -U https://github.com/raghudevopsb62/ansible roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV}"
+    ]
+  }
+}
